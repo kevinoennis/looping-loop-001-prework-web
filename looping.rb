14999bb9d789c3f9c7a111b1f218597dcda8762c
kevinoennis <kevinoennis@gmail.com>
2016-03-20 20:58:38 +0000

    Done.

--- looping.rb
+++ looping.rb
@@ -1,5 +1,12 @@
 def looping
-	#your code here
+counter = 0
+loop do
+  counter += 1
+  puts "This is count #{counter} of the loop"
+  if counter >= 10
+  break
+  end
+end
 end
 
-#call your method here
+looping
